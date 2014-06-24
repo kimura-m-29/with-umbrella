63d1807e52898e2d6fdbea328aebd79d368bb8f0
kimura-m-29 <kimura.m.29@gmail.com>
2014-06-25 02:50:40 +0900

    設定画面でiBeaconのUUIDや通知を行う距離等を設定できるようにしました。

--- www/js/app.js
+++ www/js/app.js
@@ -3,38 +3,65 @@
 
     var app = angular.module('myApp', ['onsen.directives', 'ngTouch']);
 
+    app.factory('beaconService', function ($rootScope) {
+        ibeacon.identifier = 'with-umbrella';
+        var self = this;
+
+        return {
+            startRangingBeaconsInRegion: function ($scope) {
+                ibeacon.startRangingBeaconsInRegion({
+                    region: new ibeacon.Region({
+                        uuid: $scope.setting.uuid
+                    }),
+                    didRangeBeacons: function (result) {
+                        console.log(JSON.stringify(result));
+                        var beacon = result.beacons[0];
+
+                        if (beacon.proximity == $scope.setting.proximity) {
+                            navigator.notification.vibrate(1000);
+                        }
+                    }
+                });
+            }
+        };
+    });
+
+    /**
+     * 設定画面のコントローラ
+     */
+    app.controller('SettingCtrl', function ($scope, beaconService) {
+        var setting = localStorage.getItem('setting');
+        var defaultSettingJson = {
+            uuid: 'B9407F30-F5F8-466E-AFF9-25556B57FE6D',
+            proximity: 'near',
+            area: '13',
+            chanceOfRain: '60'
+        };
+
+        if (setting) {
+            var settingJson = JSON.parse(setting);
+            $scope.setting = settingJson;
+        } else {
+            $scope.setting = defaultSettingJson;
+        }
+
+        beaconService.startRangingBeaconsInRegion($scope);
+
+        // 設定をリセットする
+        $scope.reset = function () {
+            localStorage.setItem('setting', JSON.stringify(defaultSettingJson));
+            $scope.setting = defaultSettingJson;
+        };
+
+        // 設定を保存する
+        $scope.save = function (setting) {
+            localStorage.setItem('setting', JSON.stringify(angular.copy(setting)));
+        };
+    });
+
     document.addEventListener('deviceready', function () {
         console.log('### deviceready');
         angular.bootstrap(document, ['myApp']);
-
-        var clearId = null;
-        ibeacon.identifier = 'with-umbrella';
-        ibeacon.startRangingBeaconsInRegion({
-            region: new ibeacon.Region({
-                uuid: 'B9407F30-F5F8-466E-AFF9-25556B57FE6D'
-            }),
-            didRangeBeacons: function (result) {
-                console.log(JSON.stringify(result));
-                var beacon = result.beacons[0];
-
-                // デバッグ用 TODO 削除
-                angular.element('#iBeaconLog').html('uuid: ' + beacon.uuid + '<br/>' + 'rssi: ' + beacon.rssi + '<br />' + 'proximity: ' + beacon.proximity);
-
-                // iBeaconとの距離が「すごく近い」場合、バイブレーションを鳴らす
-                // TODO 天気予報APIたたいて、降水確率がしきい値以上だったら鳴らす
-                // →朝4時とかに定期的に叩くのでも良いかも…。
-                if (beacon.proximity == 'immediate') {
-                    if (!clearId) {
-                        clearId = setInterval(function () {
-                            navigator.notification.vibrate(1000);
-                        });
-                    }
-                } else {
-                    clearInterval(clearId);
-                    clearId = null;
-                }
-            }
-        });
     }, false);
 })
 ();
