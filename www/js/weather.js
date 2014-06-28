626edf574679152126c87cbea1d057d11bfa09fd
yamagomori <say1847aiu@gmail.com>
2014-06-28 13:01:48 +0900

    #9 さくせいちゅう…
    作業途中

--- www/js/weather.js
+++ www/js/weather.js
@@ -1,7 +1,9 @@
 (function(){
 	'use strict';
 	var app = angular.module('myApp', ['onsen.directives']);
-	var WEATHER_API = "http://openweathermap.org/data/2.0/weather/city/__CITY_ID__?callback=JSON_CALLBACK";
+//	var WEATHER_API = "http://openweathermap.org/data/2.0/weather/city/__CITY_ID__?callback=JSON_CALLBACK";
+	var WEATHER_API = "http://openweathermap.org/data/2.5/weather?id=__CITY_ID__&callback=JSON_CALLBACK";
+	var CELSIUS_NUM = 273.15;
 
 	/**
 	 * コントロール
@@ -9,12 +11,13 @@
 	// お天気表示画面
 	app.controller('weatherController', ['$scope', '$http', 'weatherService', function ($scope, $http, weatherService) {
 
-		$scope.notice = "傘もってけ！";
-		$scope.place = "Tokyo, JP";
-		$scope.temp = "28";
-		$scope.prec = "30";
-		$scope.url = "http://www.google.co.jp";
+		// TODO 設定ファイルから場所のIDを取得
 		$scope.cityId = "1850147";
+
+		$scope.notice = "ローディング中…";
+//		$scope.place = "--";
+//		$scope.temp = "--";
+//		$scope.prec = "--";
 		
 		// リロードボタン
 		$scope.reload = function(){
@@ -22,8 +25,13 @@
 			// 天気情報をげっと
 			weatherService.getWeatherInfo($scope.cityId)
 				.success(function(response) {
-					$scope.resdata = response;
-					// 天気の表示によって変更
+console.log(response);
+					var $info = weatherService.loadWeatherInfo(response);
+console.log($info);
+					$scope.place = $info.name + " - " + $info.date;
+					$scope.temp = $info.temp;
+					$scope.rain = $info.rain;
+					
 				})
 				.error(function(response) {
 					alert("error!");
@@ -47,10 +55,46 @@
 				return;
 			}
 			var url = WEATHER_API.replace('__CITY_ID__', $cityId);
-
 			// データ取得
 			return $http.jsonp(url);
 		};
+
+		// 天気予報を表示用に加工
+		this.loadWeatherInfo = function( $info ){
+			if( !$info ){
+				return null;
+			};
+			var now = new Date();
+
+			var $ret = {};
+			$ret.name = $info.name;
+			$ret.date = now.getFullYear() + "/" + (now.getMonth()+1) + "/" + now.getDate();
+			$ret.temp = Math.round($info.main.temp - CELSIUS_NUM);	// セルシウスに変換
+			$ret.rain = $info.rain;
+			if( $info.snow ){
+				$ret.snow = $info.snow;
+			}
+			return $ret;
+		};
+
+		var getWeatherIcon = function( $rain, $iconId ){
+			if( $iconId in ICON_LIST ){
+				
+			}
+			// 該当アイコンがない場合、降水量が0より大きいなら雨
+			if( $rain > 0 ){
+				return WEATHER_IMG_RAIN;
+			}
+			// ここまできたらわからん
+
+		};
+
+/*		var ICON_LIST = {
+			"09d" : WEATHER_IMG_RAIN
+		
+		};
+*/
 	}]);
+
 
 })();
